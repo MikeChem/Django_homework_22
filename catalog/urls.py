7334f3dc5a86d1bfe3e8ab01c5f9963e0cf08602
Mikhail Lashukov <lashukovms@gmail.com>
2025-02-09 18:58:44 +0300

    create models product and category

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from catalog.apps import CatalogConfig
 from . import views
 
-app_name = CatalogConfig.name
+app_name = CatalogConfig
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
+    path("home/", views.home, name="home"),
+    path("catalog/", views.contacts, name="contacts"),
 ]
