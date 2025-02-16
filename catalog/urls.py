27b46e36ecf4d2345ed04329be9bf3813fe01e2b
Mikhail Lashukov <lashukovms@gmail.com>
2025-02-16 16:52:46 +0300

    new functional

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
 from . import views
+from catalog.views import products_list, products_detail
+from django.conf import settings
+from django.conf.urls.static import static
 
-app_name = CatalogConfig
+app_name = 'catalog'
 
 urlpatterns = [
     path("home/", views.home, name="home"),
-    path("catalog/", views.contacts, name="contacts"),
+    path("contacts/", views.contacts, name="contacts"),
+    path("", views.products_list, name='products_list'),
+    path("products/<int:pk>/", views.products_detail, name='products_detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
